d1ee2a9b4be06d8c1bc7af1ae67c2eafbd325d7d
yourzinc <your_zinc@ewhain.net>
2023-03-03 15:39:47 +0900

    feat: Question Field 이름 지정

--- src/main/java/com/healthier/headachelogic/domain/Question.java
+++ src/main/java/com/healthier/headachelogic/domain/Question.java
@@ -2,18 +2,29 @@ package com.healthier.headachelogic.domain;
 
 import lombok.Data;
 import lombok.Getter;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.util.List;
 
 @Getter
 @Document(collection = "question")
 public class Question {
+    @Id
+    private ObjectId _id;
 
     private int id;
     private String category; // headache
     private String type; // basic, red-flag-sign, primary-headache-c, primary-headache, pain-area
+
+    @Field(name = "pain_site")
     private String painSite;
+
+    @Field(name = "is_first")
+    private Boolean isFirst; // First Question of Pain site
+
     private String question;
     private String tag;
     private Boolean isMultiple;
