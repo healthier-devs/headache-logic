d28a5a8deb65e507759fb815b35a6e75faaebeb7
yourzinc <your_zinc@ewhain.net>
2023-03-03 13:41:00 +0900

    feat: Initialize a project

---
+++ src/main/java/com/healthier/headachelogic/HeadacheLogicApplication.java
@@ -0,0 +1,13 @@
+package com.healthier.headachelogic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeadacheLogicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeadacheLogicApplication.class, args);
+	}
+
+}
