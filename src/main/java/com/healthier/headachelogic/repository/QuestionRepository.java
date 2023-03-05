e4810d2418d80910abc8f3c14b8f29180213ae03
yourzinc <your_zinc@ewhain.net>
2023-03-05 13:38:51 +0900

    feat: Add findBy to Question Repository

--- src/main/java/com/healthier/headachelogic/repository/QuestionRepository.java
+++ src/main/java/com/healthier/headachelogic/repository/QuestionRepository.java
@@ -5,11 +5,12 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface QuestionRepository extends MongoRepository<Question, String> {
     Optional<Question> findById(@Param("id") int id);
-    Optional<Question> findByType(@Param("type") String type);
-    Optional<Question> findByPainSiteContaining(@Param("pain_site") String painSite);
+    List<Question> findByType(@Param("type") String type);
+    Optional<Question> findByPainSiteContainsAndIsFirst(@Param("pain_site") String painSite, @Param("is_first") Boolean isFirst);
 }
