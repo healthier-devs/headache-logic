de2318636d4334f8dedb2cf44ad9295c4fd9452a
offsujin <offsujin@gmail.com>
2023-03-08 21:06:15 +0700

    chore: Type Enum 생성

---
+++ src/main/java/com/healthier/headachelogic/domain/Type.java
@@ -0,0 +1,21 @@
+package com.healthier.headachelogic.domain;
+
+public enum Type {
+    BASIC("basic"),
+    REDFLAGSIGN("red-flag-sign"),
+    PRIMARYHEADACHEC("primary-headache-c"),
+    PRIMARYHEADACHE("primary-headache"),
+    PAINAREA("pain-area"),
+    ADDITIONALFACTOR("additional-factor")
+    ;
+
+    private final String label;
+
+    Type(String label) {
+        this.label = label;
+    }
+
+    public String label() {
+        return label;
+    }
+}
